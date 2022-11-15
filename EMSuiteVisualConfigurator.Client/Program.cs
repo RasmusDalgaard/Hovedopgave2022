b834356dcaf376bfe00a84c3c853684aa09b064a
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-11-15 07:28:45 +0100

    refactoring

--- EMSuiteVisualConfigurator.Client/Program.cs
+++ EMSuiteVisualConfigurator.Client/Program.cs
@@ -1,8 +1,5 @@
 using EMSuiteVisualConfigurator.Client;
-using EMSuiteVisualConfigurator.Data.Repositories;
-using EMSuiteVisualConfigurator.UseCases;
-using EMSuiteVisualConfigurator.UseCases.PluginInterfaces;
-using EMSuiteVisualConfigurator.UseCases.ToDoFeatures;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,8 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddTransient<IToDoRepository, ToDoRepository>();
-builder.Services.AddTransient<IGetToDo, GetToDo>();
+
 
 
 await builder.Build().RunAsync();
