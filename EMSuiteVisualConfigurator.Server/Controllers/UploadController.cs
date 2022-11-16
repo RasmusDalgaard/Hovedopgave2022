fa66c62079718e51353145911b30f2c95e199cc3
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-11-16 13:45:03 +0100

    add Shared-project, Server-project and a button for uploading images to the SiteCanvas

---
+++ EMSuiteVisualConfigurator.Server/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using EMSuiteVisualConfigurator.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMSuiteVisualConfigurator.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] ImageFile file)
+        {
+            var buf = Convert.FromBase64String(file.base64data);
+            await System.IO.File.WriteAllBytesAsync(env.ContentRootPath + System.IO.Path.DirectorySeparatorChar + Guid.NewGuid().ToString("N") + "-" + file.fileName, buf);
+        }
+    }
+}
