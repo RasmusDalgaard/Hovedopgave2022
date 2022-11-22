83b8f856b60d379847b9e626b2fada5b74de9ff8
Thomas Steen Amorsen <cph-ta181@cphbusiness.dk>
2022-11-22 10:54:11 +0100

    Adds restriction on Zone so that it can't be dragged back into icenMeny

--- EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/DragAndDrop.js
+++ EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/DragAndDrop.js
@@ -8,11 +8,12 @@
         listeners: {
             start(event) {
                 newComp = true
-                startPos.x, position.x = parseInt(event.target.getAttribute("x"))
-                startPos.y, position.y = parseInt(event.target.getAttribute("y"))
+                startPos.x = position.x = parseInt(event.target.getAttribute("x"))
+                startPos.y = position.y = parseInt(event.target.getAttribute("y"))
                 if (position.x != 0 && position.y != 0) {
                     newComp = false
                 }
+                console.log(startPos.x)
             },
             move(event) {
                 position.x += event.dx
@@ -20,16 +21,21 @@
                 event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
             },
             end(event) {
-                if (document.getElementsByClassName('iconmenu')[0].offsetWidth / 2 < position.x) {
+                if (document.getElementsByClassName('iconmenu')[0].offsetWidth / 2 < position.x && newComp) {
                     if (event.target.getAttribute("name") == "zone") {
                         event.target.setAttribute("class", "drag-resize zone")
                     }
                     DotNet.invokeMethodAsync("EMSuiteVisualConfigurator.Client", "addItem", event.target.getAttribute("name"), objref)
                 }
-                else {
+                else if (document.getElementsByClassName('iconmenu')[0].offsetWidth / 2 > position.x && newComp) {
                     position.x = 0
                     position.y = 0
                 }
+                else if (document.getElementsByClassName('iconmenu')[0].offsetWidth / 2 > position.x && !newComp) {
+                    position.x = startPos.x
+                    position.y = startPos.y
+                }
+                
                 event.target.setAttribute("x", position.x)
                 event.target.setAttribute("y", position.y)
                 event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
@@ -46,11 +52,14 @@
 
 function dragResize(className) {
     const position = { x: 0, y: 0 }
+    const startPos = { x: 0, y: 0 }
     interact(className).draggable({
         listeners: {
             start(event) {
-                position.x = parseInt(event.target.getAttribute("x"))
-                position.y = parseInt(event.target.getAttribute("y"))
+                position.x = startPos.x = parseInt(event.target.getAttribute("x"))
+                position.y = startPos.y = parseInt(event.target.getAttribute("y"))
+                console.log(startPos.x)
+                console.log(position.x)
             },
             move(event) {
                 position.x += event.dx
@@ -58,8 +67,13 @@ function dragResize(className) {
                 event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
             },
             end(event) {
+                if (document.getElementsByClassName('iconmenu')[0].offsetWidth / 2 > position.x) {
+                    position.x = startPos.x
+                    position.y = startPos.y
+                }
                 event.target.setAttribute("x", position.x)
                 event.target.setAttribute("y", position.y)
+                event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
             },
         }, modifiers: [
             interact.modifiers.restrictRect({
