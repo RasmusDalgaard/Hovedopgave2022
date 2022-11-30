457a7e0dadf1aa12999086c9c7285da1fdf7126a
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-11-30 11:01:47 +0100

    remove redundant imports

--- EMSuiteVisualConfigurator.CoreBusiness/Entities/EMSuiteConfiguration.cs
+++ EMSuiteVisualConfigurator.CoreBusiness/Entities/EMSuiteConfiguration.cs
@@ -1,9 +1,4 @@
 ﻿using EMSuiteVisualConfigurator.CoreBusiness.Primitives;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EMSuiteVisualConfigurator.CoreBusiness.Entities
 {
