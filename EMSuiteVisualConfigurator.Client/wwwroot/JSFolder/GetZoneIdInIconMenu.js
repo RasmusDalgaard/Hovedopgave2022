21d0f5ea6412472d9e2485075e8a9188f3c91235
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-12-15 09:31:11 +0100

    remove unused script

--- EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/GetZoneIdInIconMenu.js
+++
@@ -1,5 +0,0 @@
-﻿function GetZoneIdInIconMenu() {
-    var zoneInIconMenuId = document.getElementsByClassName("draggable").namedItem("zone").id;
-    console.log("Zone in iconMenu id = " + zoneInIconMenuId)
-    return parseInt(zoneInIconMenuId);
-}
