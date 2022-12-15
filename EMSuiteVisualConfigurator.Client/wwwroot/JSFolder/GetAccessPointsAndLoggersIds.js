ae26f20f3f2a02243bfa68997bf074f06b54f243
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-12-15 13:55:28 +0100

    looping in LinkAndAddZones & LinkAndAddAccessPoints wip

---
+++ EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/GetAccessPointsAndLoggersIds.js
@@ -0,0 +1,35 @@
+﻿function GetAccessPointsAndLoggersIds(siteId) {
+    //select all sites
+    sites = [...document.getElementsByName('site')]
+    site = sites.find(site => site.id == siteId)
+    //Select all sensors exept the last
+    sens = site.getElementsByClassName('sensor')
+    sensors = [].slice.call(sens, 0, sens.length - 1)
+    //Select all accesspoints exept the last
+    access = site.getElementsByClassName('accesspoint')
+    accesspoints = [].slice.call(access, 0, access.length - 1)
+    //Make an Empty zone dictionary <int, Array>
+    accessDict = {}
+    // add entries to accessDict where key is accesspoint ID and value is array of sensor ID's
+    sensors.forEach(function (sensor) {
+        var closestDistance = null;
+        var closestID
+        var sx = sensor.getAttribute('x')
+        var sy = sensor.getAttribute('y')
+        accesspoints.forEach(function (ap) {
+            var apX = parseInt(ap.getAttribute('x'));
+            var apY = parseInt(ap.getAttribute('y')) + 100
+            var apID = parseInt(ap.id);
+            var dist = Math.sqrt(Math.pow(apX - sx, 2) * Math.pow(apY - sy, 2))
+            if (dist < closestDistance || closestDistance == null) {
+                closestID = apID
+                closestDistance = dist
+            }
+        })
+        if (accessDict[closestID] == null) {
+            accessDict[closestID] = []
+        }
+        accessDict[closestID].push(parseInt(sensor.id))
+    })
+    return accessDict
+}
--- EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/GetZonesAndLoggersIds.js
+++ EMSuiteVisualConfigurator.Client/wwwroot/JSFolder/GetZonesAndLoggersIds.js
@@ -1,20 +1,15 @@
-﻿function GetZoneAndChannelIds() {
+﻿function GetZonesAndLoggersIds(siteId) {
+    sites = [...document.getElementsByName('site')]
+    site = sites.find(site => site.id == siteId)
     //Select all sensors exept the last
-    sens = document.getElementsByName('sensor')
+    sens = site.getElementsByClassName('sensor')
     sensors = [].slice.call(sens, 0, sens.length - 1)
-
-    //Select all accesspoints exept the last
-    access = document.getElementsByName('accesspoint')
-    accesspoints = [].slice.call(access, 0, access.length - 1)
-
     //Select all zones exept the last
-    zone = document.getElementsByName('zone')
+    zone = site.getElementsByClassName('zone')
     zones = [].slice.call(zone, 0, zone.length - 1)
-
     //Make an Empty zone dictionary <int, Array>
     zoneDict = {}
-
-    //add entries to zoneDict where key value is zone id and value in array if sensor ID's
+    //add entries to zoneDict where key  is zone id and value is array of sensor ID's
     zones.forEach(function (zone) {
         var zone1X = parseInt(zone.getAttribute('x'));
         var zone1Y = parseInt(zone.getAttribute('y')) + 200;
@@ -25,10 +20,12 @@
         sensors.forEach(function (sensor) {
             var sx = sensor.getAttribute('x');
             var sy = sensor.getAttribute('y');
+            console.log('zone2x: ' + zone2X)
+            console.log('sensorx: ' + sx)
             if (sx > zone1X & sx < zone2X & sy > zone1Y & sy < zone2Y) {
-                zoneDict[zone.id].push(parseInt(sensor.id));
+                zoneDict[zoneID].push(parseInt(sensor.id));
             }
         })
     })
-    return zoneDict;
+    return zoneDict
 }
